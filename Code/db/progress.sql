939f055630f8c4ec507df0d999316b2a10d52768
marjoriebuctolan <marjbuctolan@gmail.com>
2016-02-19 19:12:02 +0800

    [ADDED]: Table Room

--- Code/db/progress.sql
+++ Code/db/progress.sql
@@ -12,3 +12,17 @@ CREATE TABLE Customer (
 
   PRIMARY KEY(id_customer)
 );
+
+CREATE TABLE Room (
+  id_room INT,
+  room_number VARCHAR,
+  cost INT,
+  available_room INT,
+  hotel_id INT references Hotel(hotel_id),
+)
+
+CREATE TABLE Type (
+  id_room_type INT,
+  room_type VARCHAR,
+  room_id INT references Room(id_room),
+)
