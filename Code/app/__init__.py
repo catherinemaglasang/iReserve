44206da0f9c37be6a015f6990364e5559025c5a7
marjoriebuctolan <marjbuctolan@gmail.com>
2016-02-19 19:25:42 +0800

    Password

--- Code/app/__init__.py
+++ Code/app/__init__.py
@@ -6,7 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # api = Api(app)
 #Include config from config.py
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:asdasd@localhost/flask'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:marj121596@localhost/flask'
 app.secret_key = '23jDSJ32lzcxcwsSDWJK'
 app.CSRF_ENABLED = True
 #Create an instance of SQLAclhemy
