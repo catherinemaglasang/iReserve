780ddf5a04baab18e56c1b6c1c0ce3d96c91b179
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-03-07 00:11:39 +0800

    [ADDED]: newfeedback Function

--- Code/db/progress.sql
+++ Code/db/progress.sql
@@ -78,7 +78,7 @@ CREATE TABLE Type (
 );
 
 CREATE TABLE Feedback (
-  feedback_id INT PRIMARY KEY,
+  feedback_id SERIAL PRIMARY KEY,
   comment TEXT,
   created_date TIMESTAMP,
   is_active BOOLEAN,
@@ -193,7 +193,7 @@ create or replace function gethotel(OUT INT, OUT VARCHAR, OUT TEXT, OUT VARCHAR,
 $$
 
   SELECT id_hotel, hotel_name, description, email_address, address, contact_number, google_map,
-      no_of_restaurants, no_of_rooms, extra, is_active FROM Hotel;
+      no_of_restaurant, no_of_rooms, extra, is_active FROM Hotel;
 
 $$
   LANGUAGE 'sql';
@@ -203,8 +203,30 @@ create or replace function gethotel_id(IN par_id INT, OUT VARCHAR, OUT TEXT, OUT
                                         OUT INT, OUT INT, OUT TEXT, OUT BOOLEAN) RETURNS SETOF RECORD AS
 $$
 
-  SELECT hotel_name, description, email_address, address, contact_number, no_of_restaurants,
+  SELECT hotel_name, description, email_address, address, contact_number, no_of_restaurant,
       no_of_rooms, extra, is_active FROM Hotel WHERE id_hotel = par_id;
 
 $$
   LANGUAGE 'sql';
+
+
+create or replace function newfeedback(par_feedback_id INT, par_comment TEXT, par_created_date TIMESTAMP, par_is_active BOOLEAN, par_hotel_id INT) returns TEXT AS
+$$
+  DECLARE
+    loc_id INT;
+    loc_res TEXT;
+  BEGIN
+    SELECT INTO loc_id feedback_id FROM Feedback WHERE feedback_id = par_feedback_id;
+    if loc_id isnull THEN
+
+      INSERT INTO Feedback(feedback_id, comment, created_date, is_active, hotel_id) VALUES (par_feedback_id, par_comment, par_created_date, TRUE, par_hotel_id);
+
+      loc_res = 'OK';
+
+      ELSE
+        loc_res = 'ID EXISTED';
+      end if;
+      return loc_res;
+  END;
+$$
+  LANGUAGE 'plpgsql';
