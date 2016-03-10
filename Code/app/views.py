b2288567ac3af4553bc8c863d2cbbb741103581f
Neiell Care L. Paradiang <koneb2013@gmail.com>
2016-03-10 10:42:43 +0800

    [FIXED] Signup API

--- Code/app/views.py
+++ Code/app/views.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify, url_for, render_template, request
 from flask.ext.cors import CORS
 import flask
+import json
 import sys
 from flask.ext.restful.utils.cors import crossdomain
 from flask_httpauth import HTTPBasicAuth
@@ -24,14 +25,22 @@ def spcall(qry, param, commit=False):
         res = [("Error: " + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]),)]
     return res
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
-@app.route('/api/register/<string:email>/<string:password>/<string:fname>/<string:lname>/<string:contact>/<string:address>/<string:postal>/<string:gender>/<string:birthdate>', methods=['POST', 'GET'])
-def register(email, password, fname, lname, contact, address, postal, gender, birthdate):
-    res = spcall("newcustomer", (email, password, fname, lname, contact, address, postal, gender, birthdate), True)
+@app.route('/api/register', methods=['POST', 'GET'])
+def register():
+    req = request.json
+
+    res = spcall("newcustomer", (req['email'], req['password'], req['fname'], req['lname'],
+                                 req['contact'], req['address'], req['postal'], req['gender'],
+                                 req['bday']), True)
 
     if 'Error' in res[0][0]:
         return jsonify({'status': 'error', 'message': res[0][0]})
