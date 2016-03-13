99225b3551353563bfab30bb1a50eb9619baff55
Kristel Pabillaran <pabillarankristel@ymail.com>
2016-03-14 06:16:52 +0800

    [UPDATED] views.py

--- Code/app/views.py
+++ Code/app/views.py
@@ -28,7 +28,7 @@ def spcall(qry, param, commit=False):
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('test.html')
 
 @app.route("/home")
 def home():
@@ -36,14 +36,35 @@ def home():
 
 @app.route('/api/register', methods=['POST', 'GET'])
 def register():
-    req = request.json
 
-    res = spcall("newcustomer", (req['email'], req['password'], req['fname'], req['lname'],
-                                 req['contact'], req['address'], req['postal'], req['gender'],
-                                 req['bday']), True)
+    # get all the input values for client side
+    value_one = request.form.get('first')
+    value_two = request.form.get('second')
+    value_three = request.form.get('third')
+    value_four = request.form.get('fourth')
+    value_five = int( request.form.get('fifth') )
+    value_six = request.form.get('sixth')
+    value_seven = request.form.get('seventh')
+    value_eight = request.form.get('eighth')
+    value_nine = request.form.get('ninth')
 
-    if 'Error' in res[0][0]:
-        return jsonify({'status': 'error', 'message': res[0][0]})
+    # store all the collected values(from client side) to database using stored proc
+    res = spcall("newcustomer", (value_one, value_two, value_three, value_four,
+                                 value_five, value_six, value_seven, value_eight,
+                                 value_nine), True)
 
-    return jsonify({'status': 'ok', 'message': res[0][0]})
+    # print values for tracing purpose
+    print value_one
+    print value_two
+    print value_three
+    print value_four
+    print int(value_five)
+    print value_six
+    print value_seven
+    print value_eight
+    print value_nine
+    # printing the res will show if successful
+    print res
 
+    # returning a json (this will call the client side again(ajax chuchu)
+    return jsonify({'status': 'ok',})
