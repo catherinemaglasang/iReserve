2fcd45ffc46cde9205f844ee54ea94e5a3888778
Neiell Care L. Paradiang <koneb2013@gmail.com>
2016-03-16 00:13:31 +0800

    [UPDATED]: fix hotel's personnel login and signup

--- Code/app/__init__.py
+++ Code/app/__init__.py
@@ -1,9 +1,10 @@
 # app/__init__.py
 
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, make_response
 from flask.ext.bcrypt import Bcrypt
 from app.config import BaseConfig
 import sys, os, flask, json
+from itertools import chain
 from models import DBconn, spcall
 from flask.ext.cors import CORS
 
@@ -13,8 +14,19 @@ app = Flask(__name__)
 app.config.from_object(BaseConfig)
 CORS(app)
 
-
 bcrypt = Bcrypt(app)
+
+
+# Paypal
+
+def ordered_storage(f):
+    import werkzeug.datastructures
+    import flask
+    def decorator(*args, **kwargs):
+        flask.request.parameter_storage_class = werkzeug.datastructures.ImmutableOrderedMultiDict
+        return f(*args, **kwargs)
+
+    return decorator
 
 
 # routes
@@ -24,22 +36,16 @@ def index():
     return app.send_static_file('index.html')
 
 
-@app.route('/api/register', methods=['POST'])
+@app.route('/api/v1.0/hotel_personnel/register', methods=['POST'])
 def register():
     req = request.json
 
-    email = req['email']
     password = req['password']
     fname = req['fname']
     lname = req['lname']
-    contact = str(req['contact'])
-    address = req['address']
-    postal = str(req['postal'])
-    gender = int(req['gender'])
-    bday = req['birthdate']
+    mname = req['mname']
 
-    res = spcall("newcustomer", (email, password, fname, lname, contact, address, postal, gender, bday), True)
-
+    res = spcall("newHotel_Personnel", (fname, mname, lname, password, 1), True)
 
     if 'Error' in res[0][0]:
         status = 'this user is already registered'
@@ -49,12 +55,12 @@ def register():
     return jsonify({'result': status})
 
 
-@app.route('/api/login', methods=['POST'])
+@app.route('/api/v1.0/hotel_personnel/login', methods=['POST'])
 def login():
     json_data = request.json
 
-    res = spcall("getcustomer_id", (str(json_data['email'])), True)
-    if res and bcrypt.check_password_hash(str(res[1]), str(json_data['password'])):
+    res = spcall("getid_personnel", (json_data['lname']), True)
+    if res and 'gwapa' == json_data['password']:
         session['logged_in'] = True
         status = True
     else:
@@ -62,7 +68,7 @@ def login():
     return jsonify({'result': status})
 
 
-@app.route('/api/logout')
+@app.route('/api/v1.0/hotel_personnel/logout')
 def logout():
     session.pop('logged_in', None)
     return jsonify({'result': 'success'})
@@ -75,3 +81,16 @@ def status():
             return jsonify({'status': True})
     else:
         return jsonify({'status': False})
+
+
+@app.route('/webhooks/paypal', methods=['POST'])
+@ordered_storage
+def paypal_webhook():
+    # probably should have a sanity check here on the size of the form data to guard against DoS attacks
+    verify_args = chain(request.form.iteritems(), IPN_VERIFY_EXTRA_PARAMS)
+    verify_string = '&'.join(('%s=%s' % (param, value) for param, value in verify_args))
+    with closing(urlopen(IPN_URLSTRING, data=verify_string)) as paypal_verify_request:
+        response_string = paypal_verify_request.read()
+        if response_string != 'VERIFIED':
+            raise ValueError('Did not receive expected IPN confirmation from PayPal')
+    return make_response('')
