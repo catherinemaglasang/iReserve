365592883720267e6d11c0d58e6f5eb931b44e68
Neiell Care L. Paradiang <koneb2013@gmail.com>
2016-03-28 04:38:37 +0800

    [ADDED]: PAYPAL and BRAINTREE API key

--- Code/app/config.py
+++ Code/app/config.py
@@ -1,13 +1,23 @@
 # project/config.py
 
 import os
+import paypalrestsdk
+import braintree
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class BaseConfig(object):
-    SECRET_KEY = 'ASk293dSJSL12ncxhsS'
+    SECRET_KEY = 'EERwyDyEfWWO4NLFAqs8m4UZxKhZvMOsgeKqi1G0jgyREwE4LuZLC_g677uCJXcHUP7013FU65yAGoHM'
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 13
+    paypalrestsdk.configure({
+        'mode': 'sandbox',
+        'client_id': 'ARoXM7uP0pMV6szI-VGjjmBFODA98t2c2c0mcTlMorOFXC2fFCaD_KLYdvD0WQMkoNrqh83J6jPlMTs4',
+        'client_secret': 'EKnb0B6WP7Km-X47iHJx6n2xXfbb5QxlaUsuxHyNh1a2J8RfrBAaKoSNrFbHoQ1tR3giqQlqRKqcOrP-'
+    })
 
-    IPN_URLSTRING = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
-    IPN_VERIFY_EXTRA_PARAMS = (('cmd', '_notify-validate'),)
+    braintree.Configuration.configure(braintree.Environment.Sandbox,
+                                      merchant_id="2rqw2449w24rmtvm",
+                                      public_key="mbshnd4nv9236pr9",
+                                      private_key="b667f52749d1dc00cb56de3622faa868")
