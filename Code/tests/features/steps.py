4f4e589b06ba9e8b0f9fc292b7e7ff9cb2ef4a37
Neiell Care L. Paradiang <koneb2013@gmail.com>
2016-03-28 04:43:48 +0800

    [ADDED]: Feedback and Search

--- Code/tests/features/steps.py
+++ Code/tests/features/steps.py
@@ -223,3 +223,97 @@ def status_success(step):
 	world.resp = json.loads(world.response.data)
 	assert_equals(world.resp['status'], "success")
 
+
+# Create Feedback
+
+@step(u'Given I am in the hotel feedback form with details:')
+def given_i_am_in_the_hotel_feedback_form_with_details(step):
+    world.feedback = step.hashes[0]
+
+
+@step(u'When I submit the feedback form')
+def when_i_submit_the_feedback_form(step):
+    world.browser = TestApp(app)
+    world.response = world.browser.post('/api/v1.0/feedback/', world.feedback)
+
+
+@step(u'Then I should get a \'([^\']*)\' response')
+def then_i_should_get_a_group1_response(step, group1):
+    assert_equals(world.response.status_code, int(group1))
+
+
+@step(u'And a message \'([^\']*)\' are returned')
+def and_a_message_group1_are_returned(step, message):
+    resp = json.loads(world.response.body)
+    assert_equal(resp['message'], message)
+
+
+#View Feedback
+
+@step(u'Given I am in the hotel\'s page with id:')
+def given_i_am_in_the_hotel_s_page_with_id(step):
+    world.hotel_id = step.hashes[0]['hotel_id']
+
+
+@step(u'When I pressed view all feedback')
+def when_i_pressed_view_all_feedback(step):
+    world.browser = TestApp(app)
+    world.response = world.browser.get('/api/v1.0/feedback/{}/'.format(world.hotel_id))
+
+
+@step(u'And it returns all hotel\'s feedback:')
+def and_it_shows_all_hotel_s_feedback(step):
+    resp = json.loads(world.response.body)
+
+    for column in step.hashes:
+        world.comment = column['comment']
+        world.created_date = column['created_date']
+        world.id = column['id']
+        world.name = column['name']
+
+    col = [[{'comment': str(world.comment), 'created_date': str(world.created_date), 'id': int(world.id),
+             'name': str(world.name)}]]
+
+    assert_equals(col, [resp['entries']])
+
+
+#Search
+
+
+@step(u'Given I filled up search box with keyword \'([^\']*)\'')
+def given_i_filled_up_search_box_with_keyword_group1(step, group1):
+    world.search_location = group1
+
+
+@step(u'When I submit the keyword')
+def when_i_submit_the_keyword(step):
+    world.browser = TestApp(app)
+    world.response = world.browser.get('/api/v1.0/search/{}/'.format(world.search_location))
+
+
+@step(u'Then I should get a \'([^\']*)\' response')
+def then_i_should_get_a_group1_response(step, group1):
+    assert_equals(world.response.status_code, int(group1))
+
+
+@step(u'And it should return this following hotel with its detail:')
+def and_it_should_return_this_following_hotel_with_its_detail(step):
+    resp = json.loads(world.response.body)
+
+    for column in step.hashes:
+        world.id_hotel = column['id_hotel']
+        world.hotel_name = column['hotel_name']
+        world.description = column['description']
+        world.email_address = column['email_address']
+        world.address = column['address']
+        world.contact_number = column['contact_number']
+        world.no_of_restaurant = int(column['no_of_restaurant'])
+        world.no_of_rooms = int(column['no_of_rooms'])
+        world.extra = column['extra']
+
+    col = [[{'id_hotel': int(world.id_hotel), 'hotel_name': str(world.hotel_name), 'description': str(world.description),
+             'email_address': str(world.email_address), 'address': str(world.address),
+             'contact_number': str(world.contact_number), 'no_of_restaurant': int(world.no_of_restaurant),
+             'no_of_rooms': int(world.no_of_rooms), 'extra': str(world.extra)}]]
+
+    assert_equals(col, [resp['entries']])
