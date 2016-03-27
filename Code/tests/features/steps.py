8e5710df519cac9fd808469baee6635d0662866f
loreinne <estenzoloreinne@gmail.com>
2016-03-28 03:30:55 +0800

    [Added] addroom rainy case steps

--- Code/tests/features/steps.py
+++ Code/tests/features/steps.py
@@ -40,7 +40,94 @@ def message_field(step):
 	assert_equals(world.rsp['status'], "ok")
 
 
+#Adding an existing room
 
+@step(u'Given I input the following room details:')
+def given_i_input_the_following_room_details(step):
+	world.details2 = step.hashes[0]
+
+@step(u'When I click the add button')
+def when_i_click_add_button(step):
+	world.browser = TestApp(app)
+	world.room_url = '/api/room/'
+	world.response = world.app.post(world.room_url, data = json.dumps(world.details2))
+
+@step(u'Then i should get a "message" containing "ID EXISTED"')
+def message_field(step):
+	world.rsp = json.loads(world.response.data)
+	assert_equals(world.rsp['message'], "ID EXISTED")
+
+
+#Adding a room that has the field room_number empty
+
+@step(u'Given I input the following room details:')
+def input_room_details(step):
+	world.infos = step.hashes[0]
+
+
+@step(u'When i click the add button')
+def click_button(step):
+	world.browser = TestApp(app)
+	world.room_url = '/api/room/'
+	world.response = world.app.post(world.room_url, data = json.dumps(world.infos))
+
+@step(u'Then i should \'([^\']*)\' response')
+def status_code_response(step, expected_status_code):
+	assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step(u'And a message "error" are returned')
+def response_message(step):
+	world.resp = json.loads(world.response.data)
+	assert_equals(world.resp['message'], "error")
+
+
+#Adding a room that has the field room_type empty
+
+@step(u'Given I input the following room details:')
+def input_room_details(step):
+	world.infos = step.hashes[0]
+
+
+@step(u'When i click the add button')
+def click_button(step):
+	world.browser = TestApp(app)
+	world.room_url = '/api/room/'
+	world.response = world.app.post(world.room_url, data = json.dumps(world.infos))
+
+@step(u'Then i should \'([^\']*)\' response')
+def status_code_response(step, expected_status_code):
+	assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step(u'And a message "error" are returned')
+def response_message(step):
+	world.resp = json.loads(world.response.data)
+	assert_equals(world.resp['message'], "error")
+
+
+# Adding a room that has the field room_number and room_type empty
+
+@step(u'Given I input the following room details:')
+def input_room_details(step):
+	world.infos = step.hashes[0]
+
+
+@step(u'When i click the add button')
+def click_button(step):
+	world.browser = TestApp(app)
+	world.room_url = '/api/room/'
+	world.response = world.app.post(world.room_url, data = json.dumps(world.infos))
+
+@step(u'Then i should \'([^\']*)\' response')
+def status_code_response(step, expected_status_code):
+	assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step(u'And a message "error" are returned')
+def response_message(step):
+	world.resp = json.loads(world.response.data)
+	assert_equals(world.resp['message'], "error")
 
 
 
