b013a7cbabdfb1e7b7fede7b1f78951e04f023c8
loreinne <estenzoloreinne@gmail.com>
2016-03-28 03:30:01 +0800

    [Added] adding new room steps

--- Code/tests/features/steps.py
+++ Code/tests/features/steps.py
@@ -1,7 +1,8 @@
 from lettuce import step, world, before
 from nose.tools import assert_equals
+from webtest import *
 from app import app
-from app.views import *
+from app.__init__ import *
 import json
 
 
@@ -11,23 +12,32 @@ def before_all():
     world.app = app.test_client()
 
 
-@step(u'Given some rooms are in the system')
-def given_some_rooms_are_in_the_system(step):
-	ROOMS.update({'1': {'room_number': '209', 'cost': 3000, 'room_type': 'one bed'}})
-   
+#Adding a room
 
-@step(u'When I retrieve the room with an id number \'(.*)\'')
-def when_i_retrieve_the_room_number(step,id):
-    world.response = world.app.get('/api/room/{}/'.format(id))
+@step(u'Given I input the following room details:')
+def given_i_input_the_following_room_details(step):
+	world.details1 = step.hashes[0]
 
-@step(u'Then I should get a \'(.*)\' response')
-def then_i_should_get_response(step, expected_status_code):
-    assert_equals(world.response.status_code, int(expected_status_code))
+@step(u'When I click the add button')
+def when_i_click_add_button(step):
+	world.browser = TestApp(app)
+	world.room_url = '/api/room/'
+	world.response = world.app.post(world.room_url, data = json.dumps(world.details1))
 
+@step(u'Then the room is added in the system')
+def then_i_should_get_response(step):
+	assert_equals(world.response.status_code, 200)
 
-@step(u'And the following room details are returned:')
-def and_the_following_room_details(step):
-    assert_equals(step.hashes, [json.loads(world.response.data)])
+@step(u'And it should have a field "message" containing "OK"')
+def message_field(step):
+	world.rsp = json.loads(world.response.data)
+	assert_equals(world.rsp['message'], "OK")
+    
+
+@step(u'And it should have a field "status" containing "ok"')
+def message_field(step):
+	world.rsp = json.loads(world.response.data)
+	assert_equals(world.rsp['status'], "ok")
 
 
 
